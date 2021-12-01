dcf6d868ac00e38155bbf549b9b3c82fd8cd913e
Fred Luetkemeier <fredluetkemeier@gmail.com>
2021-12-01 09:37:29 -0600

    Refactor solution

--- src/Day/One.elm
+++ src/Day/One.elm
@@ -46,19 +46,16 @@ partTwo inputs =
 countPositiveDeltaRanges : List Int -> Int
 countPositiveDeltaRanges values =
     case values of
-        first :: ((second :: third :: fourth :: _) as tail) ->
+        first :: ((_ :: _ :: fourth :: _) as tail) ->
             let
-                sumA =
-                    List.sum [ first, second, third ]
+                count =
+                    if fourth > first then
+                        1
 
-                sumB =
-                    List.sum [ second, third, fourth ]
+                    else
+                        0
             in
-            if sumB > sumA then
-                1 + countPositiveDeltaRanges tail
-
-            else
-                countPositiveDeltaRanges tail
+            count + countPositiveDeltaRanges tail
 
         _ ->
             0
